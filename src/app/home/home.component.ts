3af23b1e225b776adb2fd7517fed90ddaa748f0d
Kevin Gagnon <klg73@outlook.com>
2018-04-25 15:13:39 -0400

    latest changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Employee } from '../models/employee.model';
 
 @Component({
   selector: 'home',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
-  languages = ['English', 'Spanish', 'Other' ];
+  languages = ['English', 'Spanish', 'Other'];
+  model = new Employee('Kevin', 'Kronis', false);
+
+  firstNameToUpperCase(value: string) {
+    if (value.length > 0)
+      this.model.firstName = value.charAt(0).toLocaleUpperCase() + value.slice(1);
+    else
+      this.model.firstName = value;
+  }
 }
