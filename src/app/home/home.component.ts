09eba41244e41e7bb919883bd0c4109fb93befe0
Kevin Gagnon <klg73@outlook.com>
2018-04-27 17:41:51 -0400

    more items complete

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,5 +8,14 @@ import { Employee } from '../models/employee.model';
 })
 export class HomeComponent {
   languages = ['English', 'Spanish', 'Other'];
-  model = new Employee('', '', true, '',''); 
+  model = new Employee('', '', true, '', 'default');
+  hasPrimaryLanguageError = false;
+
+  validatePrimaryLanguage(value) {
+    //console.log('lang:' + this.model.primaryLanguage); //IMPORTANT THIS PASSED ME DATA
+    if (value === 'default')
+      this.hasPrimaryLanguageError = true;
+    else
+      this.hasPrimaryLanguageError = false;
+  }
 }
