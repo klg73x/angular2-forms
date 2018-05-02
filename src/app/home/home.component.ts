9f9573d614e0b6c6da1849ad4c6818181e2efcad
Kevin Gagnon <klg73@outlook.com>
2018-05-02 09:56:35 -0400

    Before NG Bootstrap Install
    before NG Bootstrap install

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,22 +9,26 @@ import { NgForm } from '@angular/forms';
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
-  languages = ['English', 'Spanish', 'Other'];
+  languages = [];
   model = new Employee('', '', true, '', 'default');
   hasPrimaryLanguageError = false;
 
   constructor(private formPoster: FormPoster) {
-
+    this.formPoster.getLanguages()
+      .subscribe(
+        data => this.languages = data.languages,
+        err => console.log('get error: ', err)
+      );
   }
 
-  submitForm(form: NgForm){
+  submitForm(form: NgForm) {
     // validate form
     this.validatePrimaryLanguage(this.model.primaryLanguage);
     if (this.hasPrimaryLanguageError)
       return;
 
     this.formPoster.postEmployeeForm(this.model).subscribe(
-      data => console.log('success: ' , data),
+      data => console.log('success: ', data),
       err => console.log('error: ', err)
     )
   }
